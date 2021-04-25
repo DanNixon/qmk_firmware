f015ac43dece8adbd5cf35807f6f7daa7dd6aefd
Dan Nixon <dan@dan-nixon.com>
2021-04-25 22:06:29 +0100

    Use correct ctrl for side of keyboard
    Also tidy mod tap keys on diagram

--- keyboards/aya/keymaps/default/keymap.c
+++ keyboards/aya/keymaps/default/keymap.c
@@ -17,7 +17,7 @@
 #include QMK_KEYBOARD_H
 
 #define _LG_ESC LGUI_T(KC_ESC)
-#define _LC_SPC LCTL_T(KC_SPC)
+#define _RC_SPC RCTL_T(KC_SPC)
 #define _RS_BSP RSFT_T(KC_BSPC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -31,7 +31,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |-----+-----+-----+-----+-----+-----+-----+-----+-----.   .-----+-----+-----+-----+-----+-----+-----+-----+-----|
      * |     |  \  |  Z  |  X  |  C  |  V  |  B  | Alt |     |   |     | Del |  N  |  M  |  ,  |  .  |  /  |     |     |
      * '-----------------------------+-----+-----+-----+-----|   +-----+-----+-----+-----+-----------------------------'
-     *                               |Shift|G/Esc| Tab |Layer|   |     |Enter|Ct/Sp|St/BS|
+     *                               |Shift| Esc | Tab |Layer|   |     |Enter|Space|Bspce|   (tap)
+     *                               |Shift| GUI | Tab |Layer|   |     |Enter| Ctrl|Shift|   (hold)
      *                               '-----------------------'   '-----------------------'
      */
     [0] = LAYOUT(
@@ -39,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, KC_BTN2, KC_Q   , KC_W   , KC_E   , KC_R   , KC_T,                                           KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC,
         _______, KC_BTN1, KC_A   , KC_S   , KC_D   , KC_F   , KC_G,    _______,                      _______, KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, KC_NUHS,
         _______, KC_NUBS, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B,    KC_LALT, _______,    _______, KC_DEL , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, _______, _______,
-                                                     KC_LSFT, _LG_ESC, KC_TAB , MO(1)  ,    _______, KC_ENT, _LC_SPC, _RS_BSP
+                                                     KC_LSFT, _LG_ESC, KC_TAB , MO(1)  ,    _______, KC_ENT, _RC_SPC, _RS_BSP
     ),
 
     /*
