d7ba029cc427782806c73b0a280015ee73c45d04
Dan Nixon <dan@dan-nixon.com>
2021-05-07 10:21:55 +0100

    Add mouse buttons back to default layer

--- keyboards/aya/keymaps/default/keymap.c
+++ keyboards/aya/keymaps/default/keymap.c
@@ -25,9 +25,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ,-----------------------------------------.                           .-----------------------------------------.
      * |     |  `  |  1  |  2  |  3  |  4  |  5  |                           |  6  |  7  |  8  |  9  |  0  |  -  |  =  |
      * |-----+-----+-----+-----+-----+-----+-----|                           |-----+-----+-----+-----+-----+-----+-----|
-     * |     |     |  Q  |  W  |  E  |  R  |  T  |                           |  Y  |  U  |  I  |  O  |  P  |  [  |  ]  |
+     * |     | RMB |  Q  |  W  |  E  |  R  |  T  |                           |  Y  |  U  |  I  |  O  |  P  |  [  |  ]  |
      * |-----+-----+-----+-----+-----+-----+-----+-----.               .-----+-----+-----+-----+-----+-----+-----+-----|
-     * |     |     |  A  |  S  |  D  |  F  |  G  |     |               |     |  H  |  J  |  K  |  L  |  ;  |  '  |  #  |
+     * |     | LMB |  A  |  S  |  D  |  F  |  G  |     |               |     |  H  |  J  |  K  |  L  |  ;  |  '  |  #  |
      * |-----+-----+-----+-----+-----+-----+-----+-----+-----.   .-----+-----+-----+-----+-----+-----+-----+-----+-----|
      * |     |  \  |  Z  |  X  |  C  |  V  |  B  | Alt |M Lyr|   |     | Del |  N  |  M  |  ,  |  .  |  /  |     |     |
      * '-----------------------------+-----+-----+-----+-----|   +-----+-----+-----+-----+-----------------------------'
@@ -37,8 +37,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
     [0] = LAYOUT(
         _______, KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5,                                           KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL,
-        _______, _______, KC_Q   , KC_W   , KC_E   , KC_R   , KC_T,                                           KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC,
-        _______, _______, KC_A   , KC_S   , KC_D   , KC_F   , KC_G,    _______,                      _______, KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, KC_NUHS,
+        _______, KC_BTN2, KC_Q   , KC_W   , KC_E   , KC_R   , KC_T,                                           KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC,
+        _______, KC_BTN1, KC_A   , KC_S   , KC_D   , KC_F   , KC_G,    _______,                      _______, KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, KC_NUHS,
         _______, KC_NUBS, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B,    KC_LALT, MO(2)  ,    _______, KC_DEL , KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, _______, _______,
                                                      KC_LSFT, _LG_ESC, KC_TAB , MO(1)  ,    _______, KC_ENT, _RC_SPC, _RS_BSP
     ),
